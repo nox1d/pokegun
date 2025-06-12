768e3b3c73a856ba5ce51ce9a1aba2674c7f1133
nox1d <114980767+nox1d@users.noreply.github.com>
2025-06-12 08:41:27 +0800

    Fix imports in map.py

--- game_classes/map.py
+++ game_classes/map.py
@@ -1,6 +1,6 @@
 from random import randint
 from copy import deepcopy
-from ..__init__ import Tile, plains, forests, mountains, water
+from .tile import Tile, plains, forests, mountains, water
 
 class Map:
     def __init__(self, width: int, height: int) -> None:
